f659933468f3aa1a3c9458d70812ca770241c7e9
Liang Gong <lgong2000@gmail.com>
2019-10-11 16:16:32 -0500

    03_03 subscribe, localStorage, window.store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import C from './constants'
 import appReducer from './store/reducers'
-import initialState from './initialState.json'
 import { createStore } from 'redux'
+
+// parse to Object
+const initialState = localStorage['redux-store'] ?
+  JSON.parse(localStorage['redux-store']) : {}
 
 const store = createStore(appReducer, initialState)
 
-console.log('initial state', store.getState())
+// Global save the store
+// Chrome - console - >store.getState() - to view state
+// Very usefull to Debug, but do not forget to delete it for production
+// store.dispatch({type:"SET_GOAL", payload: 12}) - run dispath from Chrome console
+window.store = store
+
+store.subscribe(() => console.log(store.getState()))
+
+// Chrome - console - >localStorage - to view the data
+// Chrome - console - >localStorage.clear() - to clear the data
+store.subscribe(() => {
+  const state = JSON.stringify(store.getState())
+  localStorage['redux-store'] = state
+})
 
 store.dispatch({
   type: C.ADD_DAY,
@@ -17,4 +33,7 @@ store.dispatch({
   }
 })
 
-console.log('next state', store.getState())
+store.dispatch({
+  type: C.SET_GOAL,
+  payload: 2
+})
