e56a5064df8d365c93a60703c4e429a512bf798c
Liang Gong <lgong2000@gmail.com>
2019-10-11 14:59:33 -0500

    02_07

--- src/index.js
+++ src/index.js
@@ -1,35 +1,47 @@
 import C from './constants'
-import expect from 'expect'
-import { suggestions, fetching } from './store/reducers'
+import appReducer from './store/reducers'
+import initialState from './initialState.json'
 
-
-const action = {
-  type: C.CHANGE_SUGGESTIONS,
-  payload: ['Heavenly Ski Resort', 'Heavens Sonohara']
-}
-
-const state = {
-  fetching: true,
-  suggestions: []
-}
-
-const expectedState = {
-  fetching: false,
-  suggestions: ['Heavenly Ski Resort', 'Heavens Sonohara']
-}
-
-const actualState = {
-  fetching: fetching(state.fetching, action),
-  suggestions: suggestions(state.suggestions, action)
-}
-
-expect(actualState.suggestions).toEqual(expectedState.suggestions)
-expect(actualState.fetching).toEqual(expectedState.fetching)
+let state = initialState
 
 console.log(`
 
-  initial state: ${JSON.stringify(state)}
-  action: ${JSON.stringify(action)}
-  new goal: ${JSON.stringify(actualState)}
+  Initial state
+  ==============
+  goal: ${state.goal}
+  resorts: ${JSON.stringify(state.allSkiDays)}
+  fetching: ${state.resortNames.fetching}
+  suggestions: ${state.resortNames.suggestions}
 
-  `)
+`)
+
+state = appReducer(state, {
+  type: C.SET_GOAL,
+  payload: 2
+})
+
+state = appReducer(state, {
+  type: C.ADD_DAY,
+  payload: {
+    "resort": "Mt Shasta",
+    "date": "2016-10-28",
+    "powder": false,
+    "backcountry": true
+  }
+})
+
+state = appReducer(state, {
+  type: C.CHANGE_SUGGESTIONS,
+  payload:["Mt Tallac", "Mt Hood", "Mt Shasta"]
+})
+
+console.log(`
+
+  Next state
+  ==============
+  goal: ${state.goal}
+  resorts: ${JSON.stringify(state.allSkiDays)}
+  fetching: ${state.resortNames.fetching}
+  suggestions: ${state.resortNames.suggestions}
+
+`)
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,4 +1,5 @@
 import C from '../constants'
+import { combineReducers } from 'redux'
 
 export const goal = (state=10, action) =>
   action.type === C.SET_GOAL ? parseInt(action.payload): state
@@ -62,3 +63,25 @@ export const suggestions = (state=[], action) => {
       return state
   }
 }
+
+/*const resortNames = combineReducers({
+  fetching,
+  suggestions
+})
+
+const singleReducer = combineReducers({
+  allSkiDays,
+  goal,
+  errors,
+  resortNames
+})*/
+
+export default combineReducers({
+  allSkiDays,
+  goal,
+  errors,
+  resortNames: combineReducers({
+    fetching,
+    suggestions
+  })
+})
