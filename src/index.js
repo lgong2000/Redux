5fd622e314fecff8386df4dfffcc1df4eb41beaa
Liang Gong <lgong2000@gmail.com>
2019-10-14 09:36:42 -0500

    04_01 action creattor

--- src/index.js
+++ src/index.js
@@ -1,43 +1,16 @@
-import C from './constants'
-import storeFatory from './store'
+import storeFactory from './store'
+import { addDay, removeDay, setGoal } from './actions'
 
-const initialState = (localStorage['redux-store']) ?
-  JSON.parse(localStorage['redux-store']) : {}
+const store = storeFactory()
 
-const saveState = () => {
-  const state = JSON.stringify(store.getState())
-  localStorage['redux-store'] = state
-}
+store.dispatch(
+  addDay("Heavenly", "2016-12-22")
+)
 
-const store = storeFatory(initialState)
-store.subscribe(saveState)
+store.dispatch(
+  removeDay("2016-12-22")
+)
 
-store.dispatch({
-  type: C.ADD_DAY,
-  payload: {
-    "resort": "Mt Shasta",
-    "date": "2016-10-28",
-    "powder": true,
-    "backcountry": true
-  }
-})
-
-store.dispatch({
-  type: C.ADD_DAY,
-  payload: {
-    "resort": "Squaw Valley",
-    "date": "2016-3-28",
-    "powder": true,
-    "backcountry": false
-  }
-})
-
-store.dispatch({
-  type: C.ADD_DAY,
-  payload: {
-    "resort": "The Canyons",
-    "date": "2016-11-28",
-    "powder": false,
-    "backcountry": true
-  }
-})
+store.dispatch(
+  setGoal(55)
+)
