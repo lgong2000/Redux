af76d5940a9216496102760035536600da8f0748
Liang Gong <lgong2000@gmail.com>
2019-10-15 08:51:06 -0500

    05_03 mapDispatchToProps

--- src/components/containers/ShowErrors.js
+++ src/components/containers/ShowErrors.js
@@ -1,5 +1,25 @@
 import ShowErrors from '../ui/ShowErrors'
+import { clearError } from '../../actions'
+import { connect } from 'react-redux'
 
-export default () =>
-	<ShowErrors errors={['sample error']}
-						  onClearError={index => console.log('todo: clear error at', index)} />
+//export default () =>
+//	<ShowErrors errors={['sample error','error2']}
+//						  onClearError={index => console.log('todo: clear error at', index)} />
+
+const mapStateToProps = state => {
+	return {
+		errors: state.errors
+	}
+}
+
+const mapDispatchToProps = dispatch => {
+	return {
+		onClearError(index) {
+			dispatch(
+				clearError(index)
+			)
+		}
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShowErrors)
